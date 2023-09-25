f4b06c90ff38cbfbea027ca9be7fe09eb0c99522
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-09-26 00:13:42 +0200

    header-update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
+library.add(faMagnifyingGlass);
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
