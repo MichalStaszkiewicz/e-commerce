dc28eac7d4659dcc45e8400f3f0c85a2f97d530b
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-09-30 19:39:27 +0200

    menu-button-animation-added

--- pages/components/header_nav_button.tsx
+++ pages/components/header_nav_button.tsx
@@ -1,20 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as icons from '@fortawesome/free-solid-svg-icons';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export function HeaderNavButton({ label, icon }: { label: string, icon: boolean }) {
 
     const [isMenuOpen, setMenuOpen] = useState(false);
     const toggleMenu = () => {
-        if(icon){
+        if (icon) {
             setMenuOpen(!isMenuOpen);
         }
-       
+
     };
-    
+
     return <li className={`header_nav_button ${isMenuOpen ? 'menu-open' : ''}`} onMouseEnter={toggleMenu} onMouseLeave={toggleMenu}>
         <p>{label}</p>
         {icon && <FontAwesomeIcon icon={icons.faAngleDown} />} {isMenuOpen && (
-            <ul className="shadow header_button_menu">
+            <ul className="shadow header_button_menu animate-opacity" >
                 <li className="header-menu-label">Element 1</li>
                 <li className="header-menu-label">Element 2</li>
                 <li className="header-menu-label">Element 3</li>
