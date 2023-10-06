7fe36cd7c2df60c45c99da725e7716e0e08e9322
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-10-06 21:38:01 +0200

    fix

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,18 +1,22 @@
-import { Benefit } from "@/components/benefit";
-import { BigSale } from "@/components/big_sale";
-import { Collection } from "@/components/collection";
-import { FeaturedProducts } from "@/components/featured_products";
-import { HeaderNavButton } from "@/components/header_nav_button";
-import { HeaderNavLabel } from "@/components/header_nav_label";
-import { PrimaryButton } from "@/components/primary_button";
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as icons from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { HeaderNavButton, HeaderNavLabel } from '../components/header_nav_button';
+import React from 'react';
+import promotedProduct from "../public/images/shoes.png"
+import { PrimaryButton } from '@/components/primary_button';
+import { Benefit } from '@/components/benefit';
+import { Collection } from '@/components/collection';
+import { FeaturedProducts } from '@/components/featured_products';
+import { BigSale } from '@/components/big_sale';
+export const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
 
 
-
-export function Shop() {
     return (
-        <main className={`flex min-h-screen flex-col items-center `}>
+        <main className={`flex min-h-screen flex-col items-center  ${inter.className}`}>
             <div className='header'>
                 <div className="row-space-between" style={{ height: '200px', borderBottom: '0.1px solid #f7f7f7' }}>
 
@@ -26,11 +30,8 @@ export function Shop() {
 
                     <div style={{ width: "50%", textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
                     >
-                        <div onClick={() => {
-
-
-                        }} className="site-logo">
-                            <a style={{ letterSpacing: "6px" }} className="js-logo-clone">SHOPPERS</a>
+                        <div className="site-logo">
+                            <a href="shop" style={{ letterSpacing: "6px" }} className="js-logo-clone">SHOPPERS</a>
                         </div>
                     </div>
 
@@ -52,7 +53,34 @@ export function Shop() {
                 </div>
             </div>
             <div className='content'>
+                <div className='home-promotion-container'>
 
+                    <Image className='home-promotion-image' src={promotedProduct} alt="" />
+                    <div className='home-promotion-desc-container'>
+                        <div className='home-promotion-desc'>
+
+                            <p className='home-promotion-desc-header'> Finding Your Perfect Shoes </p>
+                            <p className='home-promotion-desc-about'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.</p>
+                            <PrimaryButton height={40} width={130} label={'SHOP NOW'} />
+                        </div>
+
+
+                    </div>
+
+                </div>
+                <div className='shopping_benefits'>
+                    <Benefit title={'FREE SHIPPING'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.'} />
+                    <Benefit title={'FREE RETURNS'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.'} />
+                    <Benefit title={'CUSTOMER SUPPORT'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.'} />
+
+                </div>
+                <div className='collections'>
+                    <Collection label={'Woman'} image={'/images/women.jpg'} />
+                    <Collection label={'Children'} image={'/images/children.jpg'} />
+                    <Collection label={'Men'} image={'/images/men.jpg'} />
+                </div>
+                <FeaturedProducts products={[{ image: "/images/cloth_1.jpg", label: "Tank top", price: 50, description: "Finding perfect product" }, { image: "/images/cloth_3.jpg", label: "Tank top", price: 50, description: "Finding perfect product" }, { image: "/images/cloth_2.jpg", label: "Tank top", price: 50, description: "Finding perfect product" }, { image: "/images/cloth_3.jpg", label: "Tank top", price: 50, description: "Finding perfect product" }, { image: "/images/shoes.png", label: "Tank top", price: 50, description: "Finding perfect product" }]} />
+                <BigSale></BigSale>
 
             </div>
             <div className='footer'>
@@ -119,5 +147,6 @@ export function Shop() {
             </div>
 
 
-        </main >)
+        </main >
+    )
 }
