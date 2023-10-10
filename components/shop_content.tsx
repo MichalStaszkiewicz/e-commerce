7909982bc1a02834dc818af237511be651557478
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-10-10 23:56:55 +0200

    shop-content-position-fixed

--- components/shop_content.tsx
+++ components/shop_content.tsx
@@ -14,12 +14,20 @@ export default function ShopContent() {
                 <div className="categories">
                     <p className="categories_header">CATEGORIES</p>
                     <ul className="list">
-                        <li className="list_element"> <div>Men</div> <p style={{color:"black",}}>(2,220)</p></li>
-                        <li className="list_element"> <div>Woman</div> <p style={{color:"black"}} >(2,550)</p></li>
-                        <li className="list_element"> <div>Children</div><p style={{color:"black"}} >(2,124)</p>
+                        <li className="list_element"> <div>Men</div> <p style={{ color: "black", }}>(2,220)</p></li>
+                        <li className="list_element"> <div>Woman</div> <p style={{ color: "black" }} >(2,550)</p></li>
+                        <li className="list_element"> <div>Children</div><p style={{ color: "black" }} >(2,124)</p>
                         </li>
                     </ul>
 
+                </div>
+
+                <div className="filters">
+
+                    <div className="container">
+                    <p className="categories_header">FILTER BY PRICE</p>
+
+                    </div>
                 </div>
             </div>
             <div className="right_column"></div>
