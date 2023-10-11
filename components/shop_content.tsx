f32d3f6ce0a085fcaf0e6e5361f993713755315f
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-10-11 21:32:26 +0200

    shop-content-categories

--- components/shop_content.tsx
+++ components/shop_content.tsx
@@ -1,37 +1,75 @@
+import { DragEventHandler, useCallback, useRef, useState } from "react";
 
 
 export default function ShopContent() {
 
+    return (
+        <div className="shop_content">
 
+            <div className="container">
+                <div className="left_column">
 
+                    <div className="categories">
+                        <p className="categories_header" style={{ marginTop: '20px' }}>CATEGORIES</p>
+                        <ul className="list">
+                            <li className="list_element"> <div>Men</div> <p style={{ color: "black", }}>(2,220)</p></li>
+                            <li className="list_element"> <div>Woman</div> <p style={{ color: "black" }} >(2,550)</p></li>
+                            <li className="list_element"> <div>Children</div><p style={{ color: "black" }} >(2,124)</p>
+                            </li>
+                        </ul>
 
+                    </div>
 
-    return <div className="shop_content">
+                    <div className="filters">
 
-        <div className="container">
-            <div className="left_column">
+                        <div className="container">
+                            <p className="categories_header">FILTER BY PRICE</p>
+                            <div className="slider_container">
 
-                <div className="categories">
-                    <p className="categories_header">CATEGORIES</p>
-                    <ul className="list">
-                        <li className="list_element"> <div>Men</div> <p style={{ color: "black", }}>(2,220)</p></li>
-                        <li className="list_element"> <div>Woman</div> <p style={{ color: "black" }} >(2,550)</p></li>
-                        <li className="list_element"> <div>Children</div><p style={{ color: "black" }} >(2,124)</p>
-                        </li>
-                    </ul>
-
-                </div>
-
-                <div className="filters">
-
-                    <div className="container">
-                    <p className="categories_header">FILTER BY PRICE</p>
+                            </div>
+                            <p style={{ marginTop: '10px' }}>$76 - $262</p>
+                            <p className="categories_header" style={{ marginTop: "20px" }}>SIZE</p>
+                            <div style={{ marginTop: "10px" }}></div>
+                            <ul>
+                                <li><div>
+                                    <input type="checkbox" id="scales" name="scales" checked />
+                                    <label >  Small (2,319)</label>
+                                </div></li>
+                                <li><div>
+                                    <input type="checkbox" id="scales" name="scales" checked />
+                                    <label >  Medium (1,282)</label>
+                                </div></li>
+                                <li><div>
+                                    <input type="checkbox" id="scales" name="scales" checked />
+                                    <label >  Large (1,392)</label>
+                                </div></li>
+                            </ul>
+                            <p className="categories_header" style={{ marginTop: "20px" }}>COLOR</p>
+                            <div style={{ marginTop: "10px" }}></div>
+                            <CategoriesColorIndicator color={'red'} label={'Red'} />
+                            <CategoriesColorIndicator color={'green'} label={'Green'} />
+                            <CategoriesColorIndicator color={'aqua'} label={'Blue'} />
+                            <CategoriesColorIndicator color={'purple'} label={'Purple'} />
+                        </div>
 
                     </div>
                 </div>
-            </div>
-            <div className="right_column"></div>
+                <div className="right_column"></div>
 
-        </div>
-    </div>
+            </div>
+        </div >)
+
+
+
+}
+
+
+function CategoriesColorIndicator({ color, label }: { color: string, label: string }): JSX.Element {
+    return (<div className="color_container" >
+        <div className="color_indicator" style={{ backgroundColor: color }}></div>
+        <div style={{ marginLeft: "10px" }}></div>
+        <p >{label} (2,429)</p>
+
+
+    </div>);
 }
