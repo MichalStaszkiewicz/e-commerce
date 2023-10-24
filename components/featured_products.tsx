479eeaae517f1af326c6bf2679d2c6c04f741bb3
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-10-24 17:20:31 +0200

    featured products class component to function

--- components/featured_products.tsx
+++ components/featured_products.tsx
@@ -1,8 +1,7 @@
+import React, { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { RefObject } from "react";
 import * as icons from "@fortawesome/free-solid-svg-icons";
 import { ProductCard } from "./product_card";
-import { useRef, useState } from "react";
 import { off } from "process";
 
 interface FeaturedProductModel {
@@ -12,150 +11,110 @@ interface FeaturedProductModel {
   price: number;
 }
 
-interface FeaturedProductState {
-  leftArrowColor: string;
-  rightArrowColor: string;
-  index: number;
-}
-
 interface FeaturedProductProps {
   products: FeaturedProductModel[];
 }
 
-export class FeaturedProducts extends React.Component<
-  FeaturedProductProps,
-  FeaturedProductState
-> {
-  listRef: React.RefObject<HTMLDivElement>;
-  timer: any;
+const FeaturedProducts: React.FunctionComponent<FeaturedProductProps> = ({
+  products,
+}) => {
+  const listRef = useRef<HTMLDivElement>(null);
+  const [index, setIndex] = useState(0);
+  const [leftArrowColor, setLeftArrowColor] = useState("#cdcfd0");
+  const [rightArrowColor, setRightArrowColor] = useState(
+    products.length > 3 ? "#5d636f" : "#cdcfd0"
+  );
 
-  constructor(props: FeaturedProductProps) {
-    super(props);
-    this.state = {
-      leftArrowColor: "#cdcfd0",
-      rightArrowColor: props.products.length > 3 ? "#5d636f" : "#cdcfd0",
-      index: 0,
-    };
-
-    this.listRef = React.createRef();
-  }
-
-  componentDidMount() {
-    this.listRef.current?.addEventListener("scroll", this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    this.listRef.current?.removeEventListener("scroll", this.handleScroll);
-  }
-
-  handleScroll = () => {
-    this.setState({
-      leftArrowColor: this.state.index > 0 ? "#5d636f" : "#cdcfd0",
-      rightArrowColor:
-        this.props.products.length > 3 &&
-        this.state.index + 3 < this.props.products.length
-          ? "#5d636f"
-          : "#cdcfd0",
-    });
+  const handleScroll = () => {
+    setLeftArrowColor(index > 0 ? "#5d636f" : "#cdcfd0");
+    setRightArrowColor(
+      products.length > 3 && index + 3 < products.length ? "#5d636f" : "#cdcfd0"
+    );
   };
 
-  handleArrowClick(offset: number) {
+  const handleArrowClick = (offset: number) => {
     const scrollSpeed = 334;
 
-    this.listRef.current!.scrollBy({ left: scrollSpeed * offset });
+    listRef.current!.scrollBy({ left: scrollSpeed * offset });
+    setIndex((prevState) => index + offset);
+  };
 
-  
-    this.setState((prevState) => ({
-      index: prevState.index + offset,
-    }));
-  }
-  render() {
-    const { products } = this.props;
-
-    return (
-      <div className="featured_products">
-        <div className="featured_products_decoration"></div>
-        <p
-          style={{
-            fontWeight: "400",
-            fontSize: "30px",
-            transform: "TranslateY(200%)",
-          }}
-        >
-          Featured Products
-        </p>
-        <div className="featured_products_list" ref={this.listRef}>
-          {products.map((item, index) => (
-            <ProductCard
-              key={index}
-              image={item.image}
-              label={item.label}
-              description={item.description}
-              price={item.price} width={300} height={400}            />
-          ))}
-        </div>
-
-        <div className="featured_products_arrows_container">
-          <FontAwesomeIcon
-            onMouseEnter={() => {
-              if (this.props.products.length > 3 && this.state.index > 0) {
-                this.setState((prevState) => ({
-                  leftArrowColor: "black",
-                }));
-              }
-            }}
-            onMouseLeave={() => {
-              if (this.props.products.length > 3 && this.state.index > 0) {
-                this.setState((prevState) => ({
-                  leftArrowColor: "#5d636f",
-                }));
-              }
-            }}
-            onClick={() => {
-              if (this.props.products.length > 3 && this.state.index > 0) {
-                this.handleArrowClick(-1);
-              }
-            }}
-            style={{ height: "25px", color: this.state.leftArrowColor }}
-            icon={icons.faArrowLeft}
-          ></FontAwesomeIcon>
-
-          <div style={{ width: "50px" }}></div>
-          <FontAwesomeIcon
-            onMouseEnter={() => {
-              if (
-                this.props.products.length > 3 &&
-                this.state.index + 3 < this.props.products.length
-              ) {
-                this.setState((prevState) => ({
-                  rightArrowColor: "black",
-                }));
-              }
-            }}
-            onMouseLeave={() => {
-              if (
-                this.props.products.length > 3 &&
-                this.state.index + 3 < this.props.products.length
-              ) {
-                this.setState((prevState) => ({
-                  rightArrowColor: "#5d636f",
-                }));
-              }
-            }}
-            onClick={() => {
-              if (
-                this.props.products.length > 3 &&
-                this.state.index + 3 < this.props.products.length
-              ) {
-                this.handleArrowClick(1);
-              }
-            }}
-            style={{ height: "25px", color: this.state.rightArrowColor }}
-            icon={icons.faArrowRight}
-          ></FontAwesomeIcon>
-        </div>
+  return (
+    <div className="featured_products">
+      <div className="featured_products_decoration"></div>
+      <p
+        style={{
+          fontWeight: "400",
+          fontSize: "30px",
+          transform: "TranslateY(200%)",
+        }}
+      >
+        Featured Products
+      </p>
+      <div className="featured_products_list" ref={listRef}>
+        {products.map((item, index) => (
+          <ProductCard
+            key={index}
+            image={item.image}
+            label={item.label}
+            description={item.description}
+            price={item.price}
+            width={300}
+            height={400}
+          />
+        ))}
       </div>
-    );
-  }
-}
-export { ProductCard };
+
+      <div className="featured_products_arrows_container">
+        <FontAwesomeIcon
+          onMouseEnter={() => {
+            if (products.length > 3 && index > 0) {
+              setLeftArrowColor("black");
+            }
+          }}
+          onMouseLeave={() => {
+            if (products.length > 3 && index > 0) {
+              setLeftArrowColor("#5d636f");
+            }
+          }}
+          onClick={() => {
+            if (products.length > 3 && index > 0) {
+              handleArrowClick(-1);
+            }
+          }}
+          style={{ height: "25px", color: leftArrowColor }}
+          icon={icons.faArrowLeft}
+        ></FontAwesomeIcon>
+
+        <div style={{ width: "50px" }}></div>
+        <FontAwesomeIcon
+          onMouseEnter={() => {
+            if (
+              products.length > 3 &&
+              index + 3 < products.length
+            ) {
+              setRightArrowColor("black");
+            }
+          }}
+          onMouseLeave={() => {
+            if (
+              products.length > 3 &&
+              index + 3 < products.length
+            ) {
+              setRightArrowColor("#5d636f");
+            }
+          }}
+          onClick={() => {
+            if (products.length > 3 && index + 3 < products.length) {
+              handleArrowClick(1);
+            }
+          }}
+          style={{ height: "25px", color: rightArrowColor }}
+          icon={icons.faArrowRight}
+        ></FontAwesomeIcon>
+      </div>
+    </div>
+  );
+};
+
+export { FeaturedProducts };
