5d2f875c005714e4bedb0a3738cadd15563bbd8e
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-11-28 14:59:39 +0100

    product card component class to function

--- components/product-card/product-card.tsx
+++ components/product-card/product-card.tsx
@@ -2,44 +2,41 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import * as icons from "@fortawesome/free-solid-svg-icons";
 import "@/components/product-card/style.scss";
-interface ProductCardProps {
+
+export default function ProductCard({
+  image,
+  label,
+  description,
+  price,
+  width,
+  height,
+}: {
   image: string;
-  label: string;
+  label: String;
   description: string;
   price: number;
   width: number;
   height: number;
-}
-interface ProductCardState {}
-export class ProductCard extends React.Component<
-  ProductCardProps,
-  ProductCardState
-> {
-  constructor(props: ProductCardProps) {
-    super(props);
-  }
-  render() {
-    const { image, label, description, price } = this.props;
-    return (
+}) {
+  return (
+    <div
+      className="product_card_container"
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+      }}
+    >
       <div
-        className="product_card_container"
         style={{
-          width: `${this.props.width}px`,
-          height: `${this.props.height}px`,
+          backgroundImage: `url(${image})`,
         }}
-      >
-        <div
-          style={{
-            backgroundImage: `url(${image})`,
-          }}
-          className="product_card_image"
-        ></div>
-        <div className="product_card_desc">
-          <p className="product_card_name">{label}</p>
-          <p className="product_card_description">{description} </p>
-          <p className="product_card_price">${price}</p>
-        </div>
+        className="product_card_image"
+      ></div>
+      <div className="product_card_desc">
+        <p className="product_card_name">{label}</p>
+        <p className="product_card_description">{description} </p>
+        <p className="product_card_price">${price}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
