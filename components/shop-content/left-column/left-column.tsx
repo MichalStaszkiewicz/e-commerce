69f0c6aae0598245573f605a17558edb6573f1de
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-11-28 07:13:12 +0100

    theme button,slider

--- components/shop-content/left-column/left-column.tsx
+++ components/shop-content/left-column/left-column.tsx
@@ -11,9 +11,10 @@ import {
   CategoriesSize,
 } from "./categories-color-indicator";
 
-import { Slider } from "antd";
+import { ConfigProvider, Slider } from "antd";
 import "@/components/shop-content/style.scss";
 import React from "react";
+import theme from "@/theme/theme_config";
 //TODO: Replace useState with keeping data in url as query
 type SliderRangeValue = {
   min: number;
@@ -38,63 +39,65 @@ export function LeftColumn() {
 
   return (
     <div className="left_column">
-      <div className="categories">
-        <p className="categories_header" style={{ marginTop: "20px" }}>
-          CATEGORIES
-        </p>
-        <ul className="list">
-          <li className="list_element">
-            <div>Men</div> <p style={{ color: "black" }}>(2,220)</p>
-          </li>
-          <li className="list_element">
-            <div>Woman</div> <p style={{ color: "black" }}>(2,550)</p>
-          </li>
-          <li className="list_element">
-            <div>Children</div>
-            <p style={{ color: "black" }}>(2,124)</p>
-          </li>
-        </ul>
-      </div>
-
-      <div className="filters">
-        <p className="categories_header">FILTER BY PRICE</p>
-
-        <Slider
-          ref={sliderRef}
-          tooltip={{ open: false }}
-          range
-          step={1}
-          min={minPrice}
-          max={maxPrice}
-          defaultValue={[price.min, price.max]}
-          onChange={onChange}
-          style={{ width: "90%" }}
-        />
-
-        <p>
-          ${`${price.min}`} - ${`${price.max}`}{" "}
-        </p>
-
-        <p className="categories_header" style={{ marginTop: "20px" }}>
-          SIZE
-        </p>
-
-        <ul className="size_list">
-          <CategoriesSize label=" Small (2,319)" />
-          <CategoriesSize label=" Medium (1,282)" />
-          <CategoriesSize label=" Large (1,392)" />
-        </ul>
-        <p className="categories_header" style={{ marginTop: "20px" }}>
-          COLOR
-        </p>
-
-        <div className="color_list" style={{}}>
-          <CategoriesColorIndicator color={"red"} label={"Red"} />
-          <CategoriesColorIndicator color={"green"} label={"Green"} />
-          <CategoriesColorIndicator color={"aqua"} label={"Blue"} />
-          <CategoriesColorIndicator color={"purple"} label={"Purple"} />
+      <ConfigProvider theme={theme}>
+        <div className="categories">
+          <p className="categories_header" style={{ marginTop: "20px" }}>
+            CATEGORIES
+          </p>
+          <ul className="list">
+            <li className="list_element">
+              <div>Men</div> <p style={{ color: "black" }}>(2,220)</p>
+            </li>
+            <li className="list_element">
+              <div>Woman</div> <p style={{ color: "black" }}>(2,550)</p>
+            </li>
+            <li className="list_element">
+              <div>Children</div>
+              <p style={{ color: "black" }}>(2,124)</p>
+            </li>
+          </ul>
         </div>
-      </div>
+
+        <div className="filters">
+          <p className="categories_header">FILTER BY PRICE</p>
+
+          <Slider
+            ref={sliderRef}
+            tooltip={{ open: false }}
+            range
+            step={1}
+            min={minPrice}
+            max={maxPrice}
+            defaultValue={[price.min, price.max]}
+            onChange={onChange}
+            style={{ width: "90%" }}
+          />
+
+          <p>
+            ${`${price.min}`} - ${`${price.max}`}{" "}
+          </p>
+
+          <p className="categories_header" style={{ marginTop: "20px" }}>
+            SIZE
+          </p>
+
+          <ul className="size_list">
+            <CategoriesSize label=" Small (2,319)" />
+            <CategoriesSize label=" Medium (1,282)" />
+            <CategoriesSize label=" Large (1,392)" />
+          </ul>
+          <p className="categories_header" style={{ marginTop: "20px" }}>
+            COLOR
+          </p>
+
+          <div className="color_list" style={{}}>
+            <CategoriesColorIndicator color={"red"} label={"Red"} />
+            <CategoriesColorIndicator color={"green"} label={"Green"} />
+            <CategoriesColorIndicator color={"aqua"} label={"Blue"} />
+            <CategoriesColorIndicator color={"purple"} label={"Purple"} />
+          </div>
+        </div>
+      </ConfigProvider>
     </div>
   );
 }
