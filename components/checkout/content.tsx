2c95f57568a6ecd6aac16a442df486ef39fe7085
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-11-29 22:58:39 +0100

    place order box implemented

--- components/checkout/content.tsx
+++ components/checkout/content.tsx
@@ -138,7 +138,91 @@ export default function CheckoutContent() {
               </div>
             </div>
             <p className="label">Your Order</p>
-            <div className="your-order-box"></div>
+            <div className="your-order-box">
+              <div className="product-list-box">
+                <table style={{ height: "auto", width: "100%" }}>
+                  <thead>
+                    <tr
+                      style={{
+                        height: "50px",
+                        width: "100%",
+                        borderBottom: "1px solid #eff1f3",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      <th style={{ textAlign: "start", padding: "5px" }}>
+                        Product
+                      </th>
+                      <th
+                        style={{
+                          textAlign: "start",
+                        }}
+                      >
+                        Total
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr
+                      style={{
+                        height: "50px",
+                        width: "100%",
+                        borderBottom: "1px solid #eff1f3",
+                      }}
+                    >
+                      <td style={{ padding: "5px" }}>Top Up T-Shirt x 1</td>
+                      <td>$250.00</td>
+                    </tr>
+                    <tr
+                      style={{
+                        height: "50px",
+                        width: "100%",
+                        borderBottom: "1px solid #eff1f3",
+                      }}
+                    >
+                      <td style={{ padding: "5px" }}>Polo Shirt x 1</td>
+                      <td>$100.00</td>
+                    </tr>
+                    <tr
+                      style={{
+                        height: "50px",
+                        width: "100%",
+                        borderBottom: "1px solid #eff1f3",
+                      }}
+                    >
+                      <td style={{ fontWeight: "bold", padding: "5px" }}>
+                        Cart Subtotal
+                      </td>
+                      <td>$350.00</td>
+                    </tr>
+                    <tr
+                      style={{
+                        fontWeight: "bold",
+                        height: "50px",
+                        width: "100%",
+                      }}
+                    >
+                      <td style={{ padding: "5px" }}>Order Total</td>
+                      <td>$350.00</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="payment-option-list">
+                <div className="payment-option-box">
+                  <p>Direct Bank Transfer</p>
+                </div>
+                <div className="payment-option-box">
+                  <p>Cheque Payment</p>
+                </div>
+                <div className="payment-option-box">
+                  <p>Paypal</p>
+                </div>
+              </div>
+              <Button type="primary" className="place-order-button">
+                PLACE ORDER
+              </Button>
+            </div>
           </div>
         </div>
       </ConfigProvider>
