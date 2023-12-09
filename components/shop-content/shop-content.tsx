271fe6a0bcffeedbd641b95037635408b83934ca
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-12-09 22:03:12 +0100

    filter product

--- components/shop-content/shop-content.tsx
+++ components/shop-content/shop-content.tsx
@@ -5,18 +5,27 @@ import "@/components/shop-content/style.scss";
 import { ShopProvider } from "@/context/shop/shop";
 import { ShopLeftColumn } from "./left-column/shop-left-column";
 import { ShopRightColumn } from "./shop-right-column/shop-right-column";
+import { useShop } from "@/hooks/use-shop";
 
 export default function ShopContent() {
   return (
     <ShopProvider>
       <div className="shop_content">
-        <div className="container">
-          <ShopLeftColumn />
-          <div className="spacer"></div>
-
-          <ShopRightColumn />
-        </div>
+        <ShopViewState></ShopViewState>
       </div>
     </ShopProvider>
+  );
+}
+function ShopViewState() {
+  const shop = useShop();
+  return shop.shopState.loading ? (
+    <p>LOADING </p>
+  ) : (
+    <div className="container">
+      <ShopLeftColumn />
+      <div className="spacer"></div>
+
+      <ShopRightColumn />
+    </div>
   );
 }
--- context/shop/shop.tsx
+++ context/shop/shop.tsx
@@ -1,14 +1,19 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { createContext } from "vm";
 
 import React from "react";
 import { ShopState, TShopContext } from "./type";
+import { useRouter, useSearchParams } from "next/navigation";
+import { parse } from "path";
 
 export const ShopContext = React.createContext<TShopContext | null>(null);
 
 export function ShopProvider({ children }: any) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [isReady, setReady] = useState(false);
   const [shopState, setState] = useState<ShopState>({
-    loading: false,
+    loading: true,
     error: false,
     products: [],
     originalProducts: [],
@@ -16,10 +21,106 @@ export function ShopProvider({ children }: any) {
     productsPerPage: 9,
     selectedCategories: [],
     selectedSizes: [],
+    minPrice: 0,
+    maxPrice: 1000,
   });
+  useEffect(() => {
+    readDataFromRoute();
+  }, []);
+  useEffect(() => {
+    if (isReady) {
+      setRouterPath();
+      setState({
+        ...shopState,
+        loading: false,
+        products: filterProducts(),
+      });
+    }
+  }, [
+    isReady,
+    shopState.minPrice,
+    shopState.maxPrice,
+    shopState.selectedSizes,
+    shopState.selectedCategories,
+  ]);
+  function setRouterPath() {
+    router.push(
+      `?size=${shopState.selectedSizes}&min=${shopState.minPrice}&max=${shopState.maxPrice}`,
+      {
+        scroll: false,
+      }
+    );
+  }
+  function setUp() {
+    let products = [
+      {
+        name: "Tank Top",
+        description: "basfbasasdfdfds",
+        image: "/images/cloth_1.jpg",
+        price: 920,
+        categories: ["men"],
+        availableSize: ["medium", "large"],
+      },
 
+      {
+        name: "Tank Top",
+        description: "basfbmfgnfasdfds",
+        image: "/images/cloth_3.jpg",
+        price: 620,
+        categories: ["men"],
+        availableSize: ["medium", "large"],
+      },
+
+      {
+        name: "Tank Top",
+        description: "basfbassdfgdfds",
+        image: "/images/cloth_1.jpg",
+        price: 220,
+        categories: ["woman"],
+        availableSize: ["medium", "large"],
+      },
+      {
+        name: "Tank Top",
+        description: "bdfbsdfbs",
+        image: "/images/cloth_2.jpg",
+        price: 120,
+        categories: ["chilren"],
+        availableSize: ["small"],
+      },
+    ];
+    setState({ ...shopState, products: products, originalProducts: products });
+  }
+  function filterProducts() {
+    console.log(shopState.originalProducts);
+    const filteredProducts = shopState.originalProducts.filter(
+      (item) =>
+        item.price >= shopState.minPrice && item.price <= shopState.maxPrice
+    );
+    return filteredProducts;
+  }
+  function readDataFromRoute() {
+    let minPrice =
+      searchParams.get("min") != null
+        ? parseInt(searchParams.get("min")!)
+        : 100;
+    let maxPrice =
+      searchParams.get("max") != null
+        ? parseInt(searchParams.get("max")!)
+        : 1000;
+    let selectedSizes =
+      searchParams.get("size") != null ? searchParams.get("size") : [];
+    setState({
+      ...shopState,
+      loading: false,
+      minPrice: minPrice,
+      maxPrice: maxPrice,
+      selectedSizes: selectedSizes as string[],
+    });
+    setReady(true);
+    setUp();
+  }
   return (
-    <ShopContext.Provider value={{ shopState, setState }}>
+    <ShopContext.Provider value={{ shopState, setState, setRouterPath }}>
       {children}
     </ShopContext.Provider>
   );
