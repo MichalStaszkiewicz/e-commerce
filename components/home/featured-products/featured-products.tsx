1f4ca292e239b32fc5974688330ea93822f0772d
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2024-02-09 18:02:03 +0100

    Add responsive view for featured products carousel

--- components/home/featured-products/featured-products.tsx
+++ components/home/featured-products/featured-products.tsx
@@ -9,8 +9,43 @@ import { CarouselArrowState, ArrowDirection } from "./enum";
 import "@/components/home/featured-products/style.scss";
 import ProductCard from "@/components/product-card/component";
 import ProductCarousel from "./product-carousel";
+import useMediaQuery from "@/hooks/use-media-query";
 
 export function FeaturedProducts({ props }: { props: FeaturedProductProps }) {
+  const isDesktop = useMediaQuery("(min-width: 1200px)");
+  const isTablet = useMediaQuery("(min-width: 1000px)");
+  const isMobile = useMediaQuery("(min-width: 767px)");
+
+  function DesktopView() {
+    return (
+      <ProductCarousel
+        props={{ products: props.products, slidesAtOnce: 3 }}
+      ></ProductCarousel>
+    );
+  }
+  function TabletView() {
+    return (
+      <ProductCarousel
+        props={{ products: props.products, slidesAtOnce: 2 }}
+      ></ProductCarousel>
+    );
+  }
+  function MobileView() {
+    return (
+      <ProductCarousel
+        props={{ products: props.products, slidesAtOnce: 1 }}
+      ></ProductCarousel>
+    );
+  }
+  function RenderView() {
+    if (isDesktop) {
+      return DesktopView();
+    } else if (isTablet) {
+      return TabletView();
+    } else {
+      return MobileView();
+    }
+  }
   return (
     <div className="featured_products">
       <p
@@ -21,9 +56,7 @@ export function FeaturedProducts({ props }: { props: FeaturedProductProps }) {
       >
         Featured Products
       </p>
-      <ProductCarousel
-        props={{ products: props.products, slidesAtOnce: 3 }}
-      ></ProductCarousel>
+      {RenderView()}
     </div>
   );
 }
