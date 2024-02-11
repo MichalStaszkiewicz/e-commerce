ff2a824842c7ee8f65c0618707830354acce3c0d
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2024-02-11 23:19:38 +0100

    Add responsive styles and variables to footer component

--- components/footer/component.tsx
+++ components/footer/component.tsx
@@ -4,9 +4,98 @@ import "@/components/footer/style.scss";
 import * as icons from "@fortawesome/free-solid-svg-icons";
 import { useState, useEffect } from "react";
 import { Divider } from "antd";
+import useMediaQuery from "@/hooks/use-media-query";
+import { breakpoints } from "@/utils/breakpoints";
 
 export function Footer() {
   const [ready, setReady] = useState(false);
+
+  const isDektop = useMediaQuery(`(min-width: ${breakpoints.lg})`);
+  const isTablet = useMediaQuery(`(min-width: ${breakpoints.md})`);
+  const isMobile = useMediaQuery(`(min-width: ${breakpoints.xs})`);
+  console.log("isDektop", isDektop);
+  console.log("isTablet", isTablet);
+  console.log("isMobile", isMobile);
+  function RenderNavigationList() {
+    if (isDektop) {
+      return ViewDesktop();
+    } else if (isTablet) {
+      return ViewTablet();
+    } else {
+      return ViewMobile();
+    }
+  }
+  function ViewDesktop() {
+    return (
+      <div className="flex-column">
+        <p className="footer-section-header">Navigations</p>
+        <div className="navigation-container">
+          <ul className="navigation-list">
+            <li className="menu-label">Sell online</li>
+            <li className="menu-label">Features</li>
+            <li className="menu-label">Shopping cart</li>
+            <li className="menu-label">Store builder</li>
+          </ul>
+          <ul className="navigation-list">
+            <li className="menu-label">Mobile commerce</li>
+            <li className="menu-label">Dropshipping</li>
+            <li className="menu-label">Website development</li>
+          </ul>
+          <ul className="navigation-list">
+            <li className="menu-label">Point of sale</li>
+            <li className="menu-label">Hardware</li>
+            <li className="menu-label">Software</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+  function ViewTablet() {
+    return (
+      <div className="flex-column">
+        <p className="footer-section-header">Navigations</p>
+        <div className="navigation-container full-row ">
+          <div className="full-row-list-wrapper">
+            <ul className="navigation-list ">
+              <li className="menu-label">Sell o nline</li>
+              <li className="menu-label">Features</li>
+              <li className="menu-label">Shopping cart</li>
+              <li className="menu-label">Store builder</li>
+              <li className="menu-label">Point of sale</li>
+              <li className="menu-label">Hardware</li>
+              <li className="menu-label">Software</li>
+            </ul>
+            <ul className="navigation-list">
+              <li className="menu-label">Mobile commerce</li>
+              <li className="menu-label">Dropshipping</li>
+              <li className="menu-label">Website development</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  function ViewMobile() {
+    return (
+      <div className="flex-column">
+        <p className="footer-section-header">Navigations</p>
+        <div className="navigation-container">
+          <ul className="navigation-list">
+            <li className="menu-label">Sell online</li>
+            <li className="menu-label">Features</li>
+            <li className="menu-label">Shopping cart</li>
+            <li className="menu-label">Store builder</li>
+            <li className="menu-label">Mobile commerce</li>
+            <li className="menu-label">Dropshipping</li>
+            <li className="menu-label">Website development</li>
+            <li className="menu-label">Point of sale</li>
+            <li className="menu-label">Hardware</li>
+            <li className="menu-label">Software</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 
   useEffect(() => {
     setReady(true);
@@ -15,113 +104,52 @@ export function Footer() {
     <div className="footer">
       <Divider></Divider>
       <div className="wrapper">
-        <div className="navigation-container">
-          <p
-            style={{
-              fontSize: "20px",
-              fontWeight: "500",
-              marginBottom: "25px",
-              color: "#25262a",
-            }}
-          >
-            Navigations
-          </p>
+        {RenderNavigationList()}
+        <div className="footer-promo-wrapper">
+          <p className="footer-section-header">Promo</p>
+          <img className="promo-image-footer" src="/images/hero_1.jpg" alt="" />
 
-          <ul className="navigation-list">
-            <li className="menu-label">Sell online</li>
-            <li className="menu-label">Features</li>
-            <li className="menu-label">Shopping cart</li>
-            <li className="menu-label">Store builder</li>
-          </ul>
+          <h3>Finding Your Perfect Shoes</h3>
+          <p
+            style={{
+              fontSize: "15px",
+              fontWeight: "300",
+              color: "#8c92a0",
+            }}
+          >
+            Promo from january 15 — 25, 2019
+          </p>
         </div>
-        <div className="navigation-container">
-          <div style={{ height: "35px" }}></div>
+        <div className="footer-contact-wrapper">
+          <p className="footer-section-header">Contact Info</p>
+          <ul className="contact-list">
+            <li className="address">
+              {" "}
+              <div className="icon-wrapper">
+                {" "}
+                <FontAwesomeIcon className="icon" icon={icons.faLocationPin} />
+              </div>
+              <p style={{}}>
+                203 Fake St. Mountain View, San Francisco, California, USA
+              </p>
+            </li>
+            <li className="phone">
+              <div className="icon-wrapper">
+                {" "}
+                <FontAwesomeIcon className="icon" icon={icons.faPhone} />
+              </div>
 
-          <ul className="navigation-list">
-            <li className="menu-label">Mobile commerce</li>
-            <li className="menu-label">Dropshipping</li>
-            <li className="menu-label">Website development</li>
+              <p style={{}}>+2 392 3929 210</p>
+            </li>
+            <li className="email">
+              <div className="icon-wrapper">
+                {" "}
+                <FontAwesomeIcon className="icon" icon={icons.faEnvelope} />
+              </div>
+
+              <p style={{}}>emailaddress@domain.com</p>
+            </li>
           </ul>
-        </div>
-        <div className="navigation-container">
-          <div style={{ height: "35px" }}></div>
-          <ul className="navigation-list">
-            <li className="menu-label">Point of sale</li>
-            <li className="menu-label">Hardware</li>
-            <li className="menu-label">Software</li>
-          </ul>
-        </div>
-        <div className="navigation-container">
-          <p
-            style={{
-              fontSize: "20px",
-              fontWeight: "500",
-              marginBottom: "25px",
-              color: "#25262a",
-            }}
-          >
-            Promo
-          </p>
-          <img
-            src="/images/hero_1.jpg"
-            alt=""
-            style={{ height: "120px", width: "250px" }}
-          />
-          <div>
-            <p
-              style={{
-                fontSize: "20px",
-                fontWeight: "400",
-                color: "#7c74ea",
-              }}
-            >
-              Finding Your Perfect Shoes
-            </p>
-            <p
-              style={{
-                fontSize: "15px",
-                fontWeight: "300",
-                color: "#8c92a0",
-              }}
-            >
-              Promo from january 15 — 25, 2019
-            </p>
-          </div>
-        </div>
-        <div className="navigation-container">
-          <p
-            style={{
-              fontSize: "20px",
-              fontWeight: "500",
-              marginBottom: "25px",
-              color: "#25262a",
-            }}
-          >
-            Contact Info
-          </p>
-          <div className="contact-info-label">
-            <FontAwesomeIcon
-              style={{ color: "#7c74ea", height: "55%", marginRight: "2%" }}
-              icon={icons.faLocationPin}
-            />
-            <p style={{ width: "100%" }}>
-              203 Fake St. Mountain View, San Francisco, California, USA
-            </p>
-          </div>
-          <div className="contact-info-label">
-            <FontAwesomeIcon
-              style={{ color: "#7c74ea", height: "55%", marginRight: "2%" }}
-              icon={icons.faPhone}
-            />
-            <p style={{ width: "100%" }}>+2 392 3929 210</p>
-          </div>
-          <div className="contact-info-label">
-            <FontAwesomeIcon
-              style={{ color: "#7c74ea", height: "55%", marginRight: "2%" }}
-              icon={icons.faEnvelope}
-            />
-            <p style={{ width: "100%" }}>emailaddress@domain.com</p>
-          </div>
         </div>
       </div>
     </div>
