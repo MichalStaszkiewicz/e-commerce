384fd8c9379325d34d07bcc98898f0984f88e043
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2024-02-13 14:04:19 +0100

    Refactor search box and header styles

--- components/header/search-box/component.tsx
+++ components/header/search-box/component.tsx
@@ -4,14 +4,10 @@ export default function HeaderSearchBox() {
   return (
     <div className="block-1">
       <div className="search-box">
-      <button className="icon-search">
-          <FontAwesomeIcon
-            icon={icons.faMagnifyingGlass}
-            style={{  }}
-          />
+        <button className="icon-search">
+          <FontAwesomeIcon icon={icons.faMagnifyingGlass} style={{}} />
         </button>
         <input type="text" placeholder="Search" />
-       
       </div>
     </div>
   );
